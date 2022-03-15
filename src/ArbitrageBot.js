f888d2d70ec4d11f024208a178190da8d8c0e3cf
Zackary Scott <zack.scott9889@gmail.com>
2022-03-15 00:10:33 -0400

    Switched the SushiSwap and Uniswap .js files to be classes for the ArbitrageBot.js file to call from. Future steps would be to implement function in SushiSwap to return the price needed for comparison. We are always comparing the token that is worth less over the token that is worth more i.e. USDT/WETH where 1 WETH equals 2580 USDT at the time of this comment. This way we will return apples to apples comparison. Once we get the functions returning the values we need, we will need to implement some type of check. I will probably figure out the amount diferrences and then convert to USD for comparison. I would potentially try to make the trade if we could profit a certain amount from the trade.

---
+++ src/ArbitrageBot.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const UniswapV3PriceCalculator = require('./UniswapPriceCalculator')
+const SushiSwapPriceCalculator = require('./SushiSwapPriceCalculator')
+
+
+const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+const WBTC = '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
+const USDT = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+
+const web3 = new Web3(new HDWalletProvider(process.env.PRIVATE_KEY,process.env.RPC_URL))
+let uniswapPriceCalc = new UniswapV3PriceCalculator(web3)
+let sushiSwapPriceCalc = new SushiSwapPriceCalculator(web3)
+let main = async () => {
+    await uniswapPriceCalc.main()
+    await sushiSwapPriceCalc.main()
+    process.exit()
+}
+
+main()
