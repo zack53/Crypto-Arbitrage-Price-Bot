867d8ccc750d1c2d04b393a7284f20ae22f1a7a2
zack53 <zack@DESKTOP-6KHUI2T.localdomain>
2022-03-21 23:28:49 -0400

    Cleaned up ArbitrageBot.js file. TODO: Start working on getting the fees and pricing narrowed down for profitable trades.

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -1,15 +1,11 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const SushiSwapSingleSwap = await ethers.getContractFactory('SushiSwapSingleSwap')
-  const sushiSwapSingleSwap = await SushiSwapSingleSwap.deploy("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D");
+  const AaveFlashLoan = await ethers.getContractFactory('AaveFlashLoan')
+  const aaveFlashLoan = await AaveFlashLoan.deploy("0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5");
 
-  // We get the contract to deploy
-  const UniSwapSingleSwap = await ethers.getContractFactory("UniSwapSingleSwap");
-  const uniSwapSingleSwap = await UniSwapSingleSwap.deploy("0xE592427A0AEce92De3Edee1F18E0157C05861564");
 
-  console.log("UniSwapSingleSwap deployed to:", uniSwapSingleSwap.address);
-  console.log("SushiSwapSingleSwap deployed to:", sushiSwapSingleSwap.address);
+  console.log("AaveFlashLoan deployed to:", aaveFlashLoan.address);
 }
 
 main()
